dc7a696d6e6d2d985a83db4383d8ca856d16f94b
andreakao <andreaperez.gz@gmail.com>
2017-12-14 11:22:25 -0300

    subiendo

---
+++ asset/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('.container1').fadeOut(3300);
+   $(".container2").hide();
+});
+
+$(document).ready(function(){
+   $('.container2').delay(3100).fadeIn("slow");
+});  
+
