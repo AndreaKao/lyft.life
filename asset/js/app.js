234909e59f9e3ca16350475fcdaf16eff3785968
AndreaKao <andreaperez.gz@gmail.com>
2018-01-02 23:59:18 -0300

    cambios

--- asset/js/app.js
+++ asset/js/app.js
@@ -7,3 +7,20 @@ $(document).ready(function(){
    $('.container2').delay(3100).fadeIn("slow");
 });  
 
+$('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+      
+
+
